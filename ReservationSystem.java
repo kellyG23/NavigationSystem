946a34cd6c8006a8f1eae1d844e6b36a7ec44b0f
acxion0506 <cjondiziii@gmail.com>
2025-06-11 15:51:59 +0800

    Added a ReservationSystem

--- ReservationSystem.java
+++ ReservationSystem.java
@@ -1,4 +1,21 @@
-//public class ReservationSystem extends JFrame {
-//    ReservationSystem() {
-//    }
-//}
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ReservationSystem extends JFrame {
+    public ReservationSystem() {
+        setTitle("Campus Map");
+        setSize(500, 750);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(true);
+
+        // Make frame visible
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(ReservationSystem::new);
+    }
+}
